6872fd86688e038797186b54f0f1b858c520cf28
daisei.109498 <r_okano@daiseilog.co.jp>
2021-09-25 13:34:38 +0900

    計算アプリ完成

--- app/src/main/java/jp/techacademy/rie/okano/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/rie/okano/calcapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
 import android.util.Log
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -20,10 +21,42 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        val intent = Intent(this, SecondActivity::class.java)
-        intent.putExtra("VALUE1", editText.text.toString().toFloat())
-        intent.putExtra("VALUE2", editText2.text.toString().toFloat())
-        Log.d("UI_PARTS", editText2.text.toString())
-        startActivity(intent)
+        if (editText.text.toString() == "" || editText2.text.toString() == "") {
+            if (v != null) {
+                Snackbar.make(v, "２つの数字を入力してボタンを押してください", Snackbar.LENGTH_INDEFINITE)
+                    .setAction("OK"){
+                        Log.d("UI_PARTS", "数字の未入力があります")
+                    }.show()
+            }
+        }else {
+            val num1 =  editText.text.toString().toFloat()
+            val num2 =  editText2.text.toString().toFloat()
+            var ans = 0.0F
+                if (v != null) {
+                    when (v.id) {
+                        R.id.button -> {
+                            ans = num1 + num2
+                        }
+                        R.id.button2 -> {
+                            ans = num1 - num2
+                        }
+                        R.id.button3 -> {
+                            ans = num1 / num2
+                        }
+                        R.id.button4 -> {
+                            ans = num1 * num2
+                        }
+                        else -> {
+                            //
+                        }
+                    }
+                    val intent = Intent(this, SecondActivity::class.java)
+                    intent.putExtra("VALUE1", ans)
+                    startActivity(intent)
+                }
+            }
+        }
     }
-}
+
+
+
